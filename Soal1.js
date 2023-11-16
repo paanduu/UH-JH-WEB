8d2744dad5f8a6336307857f6b35b7c196fe308f
paanduu <132627482+paanduu@users.noreply.github.com>
2023-11-16 10:28:53 +0700

    Add files via upload

---
+++ Soal1.js
@@ -0,0 +1,21 @@
+// Tipe data angka (number)
+const angka = 42;
+console.log("Tipe data angka:", angka);
+
+// Tipe data string
+const teks = "Hello, World!";
+console.log("Tipe data string:", teks);
+
+// Tipe data array
+const array = [1, 2, 3, 4, 5];
+console.log("Tipe data array:", array);
+
+// Tipe data objek (object)
+const objek = { nama: "John", usia: 30, pekerjaan: "Programmer" };
+console.log("Tipe data objek:", objek);
+
+// Tipe data boolean
+const benar = true;
+const salah = false;
+console.log("Tipe data boolean (benar):", benar);
+console.log("Tipe data boolean (salah):", salah);
---
+++ Soal2.js
@@ -0,0 +1,9 @@
+var hari = "senin";
+
+switch (hari) {
+  case "senin":
+    console.log("Saya pilih hari sabtu");
+    break;
+  default:
+    console.log("Hari yang saya pilih bukan sabtu");
+}
